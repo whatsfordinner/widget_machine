f8e13f743903f225f875c21946a60360f84e64ad
what's for dinner <cruelangeleo7@gmail.com>
2020-04-20 22:08:10 +1000

    moved db connection and queries to its own class, implemented GETs for widgets endpoint

--- api/api/db.py
+++ api/api/db.py
@@ -3,16 +3,38 @@ import mysql.connector
 from mysql.connector import errorcode
 from flask import current_app, g
 
+class DatabaseConnection:
+    def __init__(self):
+        self.cnx = mysql.connector.connect(
+            user=current_app.config['DB_USER'],
+            password=current_app.config['DB_PASS'],
+            host=current_app.config['DB_HOST'],
+            database=current_app.config['DB_NAME']
+        )
+    
+    def close(self):
+        self.cnx.close()
+
+    def execute_query(self, query, params=None):
+        cursor = self.cnx.cursor(
+            dictionary=True
+        )
+
+        cursor.execute(query, params)
+
+        results = []
+        for result in cursor:
+            results.append(result)
+
+        cursor.close()
+
+        return results
+
 def get_db():
     if 'db' not in g:
         logging.info('Establishing connection to DB')
         try:
-            g.db = mysql.connector.connect(
-                user=current_app.config['DB_USER'],
-                password=current_app.config['DB_PASS'],
-                host=current_app.config['DB_HOST'],
-                database=current_app.config['DB_NAME']
-            )
+            g.db = DatabaseConnection()
 
         except mysql.connector.Error as err:
             logging.error(f'Unable to connect to DB: {err}')
--- api/api/widgets.py
+++ api/api/widgets.py
@@ -1,12 +1,18 @@
 import logging
 from flask import abort, Blueprint
+from . import db
 
 bp = Blueprint('widgets', __name__, url_prefix='/widgets')
 
 @bp.route('/', methods=['GET'])
 def get_widgets():
     logging.debug('incoming request: GET /widgets')
-    abort(501)
+    db_conn = db.get_db()
+    query = 'SELECT widgetid, widgetname FROM widgets'
+    result = db_conn.execute_query(query)
+    return {
+        'widgets': result
+    }
 
 @bp.route('/', methods=['POST'])
 def new_widget():
@@ -16,7 +22,19 @@ def new_widget():
 @bp.route('/<int:widget_id>', methods=['GET'])
 def get_widget(widget_id):
     logging.debug(f'incoming request: GET /widgets/{widget_id}')
-    abort(501)
+    db_conn = db.get_db()
+    query = 'SELECT widgetid, widgetname FROM widgets WHERE widgetid = %(widgetid)s'
+    params = {
+        'widgetid': widget_id
+    }
+    result = db_conn.execute_query(query, params=params)
+
+    if len(result) == 0:
+        abort(404)
+    else:
+        return {
+            'widget': result[0]
+        }
 
 @bp.route('/<int:widget_id>', methods=['PUT'])
 def update_widget(widget_id):
