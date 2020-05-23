aab2bf3e100ac62f14d03b557629f729e8bc799d
what's for dinner <cruelangeleo7@gmail.com>
2020-05-24 00:24:09 +1000

    getting tests working again

--- api/tests/test_widgets.py
+++ api/tests/test_widgets.py
@@ -1,10 +1,22 @@
 import unittest
 from unittest.mock import MagicMock, patch
-from api import widgets, db
+from api import db
 
-@patch('api.widgets.get_widgets_from_db')
+class MockDB:
+    def __init__(self, return_value):
+        self.return_value = return_value
+    
+    def get_widgets(self):
+        return self.return_value
+
+    def get_widget_by_id(self, **kwargs):
+        return self.return_value
+
+@patch('api.db.get_db')
 class GetWidgetsTestCase(unittest.TestCase):
+
     def test_get_widgets(self, mock_db):
+        from api.widgets.routes import get_widgets
         db_return = [
             {
                 'id': 1,
@@ -22,36 +34,36 @@ class GetWidgetsTestCase(unittest.TestCase):
             'widgets': db_return
         }
 
-        mock_db.return_value = db_return
-        result = widgets.get_widgets()
+        mock_db.return_value = MockDB(db_return)
+        result = get_widgets()
         self.assertDictEqual(result, expect)
     
-@patch('api.widgets.abort')
-@patch('api.widgets.get_widgets_from_db')
+@patch('api.widgets.routes.abort')
+@patch('api.db.get_db')
 class GetWidgetTestCase(unittest.TestCase):
     def test_widget_exists(self, mock_db, mock_abort):
-        db_return = [
-            {
-                'id': 1,
-                'name': 'foo',
-                'quantity': 3
-            }
-        ]
-
-        expect = {
-            'widget': db_return[0]
+        from api.widgets.routes import get_widget
+        db_return = {
+            'id': 1,
+            'name': 'foo',
+            'quantity': 3
         }
 
-        mock_db.return_value = db_return
-        result = widgets.get_widget(1)
+        expect = {
+            'widget': db_return
+        }
+
+        mock_db.return_value = MockDB(db_return)
+        result = get_widget(1)
         self.assertDictEqual(result, expect)
 
     def test_widget_nonexistent(self, mock_db, mock_abort):
+        from api.widgets.routes import get_widget
         db_return = []
-        mock_db.return_value.execute_query.return_value = db_return
-        result = widgets.get_widget(1)
+        mock_db.return_value = MockDB(db_return)
+        result = get_widget(1)
         self.assertIsNone(result)
-        mock_abort.assert_called_with(404)
+        mock_abort.assert_called_with(404, description='widget with ID 1 not found')
 
 class NewWidgetTestCase(unittest.TestCase):
     def test_no_widget_data(self):
