200613b82358637f8180d45fa79abd29d283ac44
what's for dinner <cruelangeleo7@gmail.com>
2020-05-24 22:47:59 +1000

    adding schema validation using jsonschema lib

--- api/api/widgets/routes.py
+++ api/api/widgets/routes.py
@@ -1,7 +1,7 @@
 import logging
 from flask import abort, Blueprint, request
 from api import db
-from api.widgets import utils
+from api.widgets import utils, exceptions
 
 bp = Blueprint('widgets', __name__, url_prefix='/widgets')
 logger = logging.getLogger(__name__)
@@ -32,39 +32,28 @@ def get_widget(widget_id):
 def new_widget():
     logger.debug('incoming request: POST /widgets')
     post_data = request.get_json()
-    logger.debug(f'new widget POST data: {post_data}')
 
-    if post_data is None or 'widget' not in post_data:
-        abort(422, description='data object did not contain \'widget\' object')
+    try:
+        utils.validate_data('post_widget', post_data)
+
+        # check to see if a quantity was specified
+        if 'quantity' in post_data:
+            quantity = post_data['quantity']
+        else:
+            quantity = 0
     
-    widget_data = post_data['widget']
-
-    # make sure a name has been specified and it's a string
-    if 'name' not in widget_data:
-        abort(400, description='\'widget\' object did not have \'name\' attribute')
-
-    if not isinstance(widget_data['name'], str):
-        abort(422, 'widget name must be string')
-
-    # check to see if a quantity was specified
-    if 'quantity' in widget_data:
-        # is the specified quantity an integer
-        if not isinstance(widget_data['quantity'], int):
-            abort(422, 'quantity must be integer')
-        quantity = widget_data['quantity']
-    else:
-        quantity = 0
+    except exceptions.WidgetError as err:
+        abort(err.status_code, err.message)
     
     # check to see if a widget with that name already exists
-    result = db.get_db().get_widget_by_name(widget_name=widget_data['name'])
+    result = db.get_db().get_widget_by_name(widget_name=post_data['name'])
     if result is not None:
-        abort(422, f'widget with name \'{widget_data["name"]}\' already exists')
-
+        abort(422, f'widget with name \'{post_data["name"]}\' already exists')
     
     # widget doesn't exist arleady so let's insert it
-    new_id = db.get_db().insert_widget(widget_name=widget_data['name'], widget_quantity=quantity)
+    new_id = db.get_db().insert_widget(widget_name=post_data['name'], widget_quantity=quantity)
 
-    # get newly created widget and return it
+    # get newly created widget by its id and return it
     result = db.get_db().get_widget_by_id(widget_id=new_id)
     return {
         'widget': result
--- api/api/widgets/utils.py
+++ api/api/widgets/utils.py
@@ -1,6 +1,21 @@
 import logging
+import json
+import jsonschema
+from api.widgets.exceptions import WidgetError
 
 logger = logging.getLogger(__name__)
 
-def validate_post_data(data_object=None, required_attributes=[]):
-    pass
+def get_schema(schema_name):
+    schema_file_location = f'api/widgets/schemas/{schema_name}.json'
+    logging.debug(f'reading in schema file: {schema_file_location}')
+    with open(schema_file_location) as schema_file:
+        schema_json = json.load(schema_file)
+    
+    return schema_json
+
+def validate_data(schema_name, post_data):
+    schema = get_schema(schema_name)
+    try:
+        jsonschema.validate(post_data, schema)
+    except jsonschema.ValidationError as err:
+        raise WidgetError(400, err.message)
